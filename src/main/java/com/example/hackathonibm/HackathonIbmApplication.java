f2f01a925d48646de489fcdf7f55092f7fb43b35
josh <joshua_soetan@yahoo.com>
2025-02-08 11:16:03 +0000

    Initial commit for development branch

---
+++ src/main/java/com/example/hackathonibm/HackathonIbmApplication.java
@@ -0,0 +1,14 @@
+package com.example.hackathonibm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackathonIbmApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(HackathonIbmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/hackathonibm/HackathonIbmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hackathonibm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackathonIbmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
