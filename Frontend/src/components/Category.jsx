15fce73a997b6779703e048e22988c7f3775fb10
ImPanyAngel <angelides.pany04@gmail.com>
2025-02-09 09:54:05 +0000

    fixed

--- Frontend/src/components/Category.jsx
+++ Frontend/src/components/Category.jsx
@@ -4,6 +4,19 @@ function Category() {
 
   return (
     <div className='category-container'>
+        <div className="header">
+            <h1 className="title">My Title</h1>
+
+            <div className="controls">
+                <select>
+                    <option value="daily">Daily</option>
+                    <option value="weekly">Weekly</option>
+                    <option value="monthly">Monthly</option>
+                    <option value="annual">Annual</option>
+                </select>
+                <input type="text" placeholder="Money" />
+            </div>
+        </div>
     </div>
   );
 }
